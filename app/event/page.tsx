65559f09e9ff2b18b79ff79aa5b55bff1aad4777
Specnr <specnrr@gmail.com>
2025-07-15 20:51:52 -0400

    handle time limit and player-limited matches

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -4,13 +4,13 @@ import { Container } from "@/components/Container";
 import { BaseResults, PageType } from "@/utils/interfaces/Event";
 import { getResultsFromEventId } from "@/utils/ui/requests";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
 import { SimpleResults } from "@/components/Results/SimpleResults";
 import { DetailedResults } from "@/components/Results/DetailedResults";
 
 export default function TargetPage() {
   const router = useRouter()
-  const searchParams = useSearchParams ()
+  const searchParams = useSearchParams()
   const eventId = searchParams.get("id")
   const [data, setData] = useState<BaseResults | null>(null)
   const [pageType, setPageType] = useState<PageType>(PageType.Simple)
@@ -34,13 +34,11 @@ export default function TargetPage() {
     return <Spinner />;
   }
 
-  console.log(data)
-
   return (
     <div className="w-full grid grid-cols-6 gap-4">
       <div className="col-span-6 md:col-start-2 md:col-span-4">
         <Container>
-          <h1 className="text-2xl md:text-3xl font-semibold text-center">{ data.name }</h1>
+          <h1 className="text-2xl md:text-3xl font-semibold text-center">{data.name}</h1>
           <div className="invisible h-0 md:h-fit md:visible md:my-4 text-xl grid grid-cols-3 divide-x divide-gray-700 text-center">
             <span onClick={() => setPageType(PageType.Simple)}
               className={`hover:font-bold ${pageType === PageType.Simple ? "font-bold" : null}`}>
